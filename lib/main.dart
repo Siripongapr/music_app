dcf85a017f41148f9df0252b5c2c95774b2f1f35
Siripongapr <siripong.api43@gmail.com>
2024-07-07 23:05:01 +0700

    feat: Implement simple play,pause music.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:music_app/widget/player.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:music_app/data.dart';
 import 'package:music_app/widget/draggable_sheet.dart';
+import 'package:music_app/widget/player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,69 +35,64 @@ class Playlist extends StatefulWidget {
 class _PlaylistState extends State<Playlist> {
   final DraggableScrollableController sheetController =
       DraggableScrollableController();
-  List<Map<String, dynamic>> songs = [
-    {
-      'song': 'Song 1',
-      'artist': 'Artist 1',
-      'image': 'https://mpics.mgronline.com/pics/Images/566000000527601.JPEG',
-      'status': false
-    },
-    {
-      'song': 'Song 2',
-      'artist': 'Artist 2',
-      'image':
-          'https://m.media-amazon.com/images/M/MV5BNDFjYTIxMjctYTQ2ZC00OGQ4LWE3OGYtNDdiMzNiNDZlMDAwXkEyXkFqcGdeQXVyNzI3NjY3NjQ@._V1_FMjpg_UX1000_.jpg',
-      "status": false
-    },
-    {
-      'song': 'Song 3',
-      'artist': 'Artist 3',
-      'image':
-          'https://static1.srcdn.com/wordpress/wp-content/uploads/2022/01/attack-on-titan-warrior-characters-season-4.jpg',
-      'status': false
-    },
-    {
-      'song': 'Song 4',
-      'artist': 'Artist 4',
-      'image': 'https://mpics.mgronline.com/pics/Images/566000000527601.JPEG',
-      'status': false
-    },
-    {
-      'song': 'Song 5',
-      'artist': 'Artist 5',
-      'image':
-          'https://m.media-amazon.com/images/M/MV5BNDFjYTIxMjctYTQ2ZC00OGQ4LWE3OGYtNDdiMzNiNDZlMDAwXkEyXkFqcGdeQXVyNzI3NjY3NjQ@._V1_FMjpg_UX1000_.jpg',
-      "status": false
-    },
-    {
-      'song': 'Song 6',
-      'artist': 'Artist 6',
-      'image':
-          'https://static1.srcdn.com/wordpress/wp-content/uploads/2022/01/attack-on-titan-warrior-characters-season-4.jpg',
-      'status': false
-    },
-    {
-      'song': 'Song 7',
-      'artist': 'Artist 7',
-      'image': 'https://mpics.mgronline.com/pics/Images/566000000527601.JPEG',
-      'status': false
-    },
-    {
-      'song': 'Song 8',
-      'artist': 'Artist 8',
-      'image':
-          'https://m.media-amazon.com/images/M/MV5BNDFjYTIxMjctYTQ2ZC00OGQ4LWE3OGYtNDdiMzNiNDZlMDAwXkEyXkFqcGdeQXVyNzI3NjY3NjQ@._V1_FMjpg_UX1000_.jpg',
-      "status": false
-    },
-    {
-      'song': 'Song 9',
-      'artist': 'Artist 9',
-      'image':
-          'https://static1.srcdn.com/wordpress/wp-content/uploads/2022/01/attack-on-titan-warrior-characters-season-4.jpg',
-      'status': false
-    },
-    // Add more songs here
-  ];
+  final player = AudioPlayer();
+  final playlist = ConcatenatingAudioSource(
+    useLazyPreparation: true,
+    shuffleOrder: DefaultShuffleOrder(),
+    children: [
+      AudioSource.uri(Uri.parse(
+          'https://archive.org/download/attack-on-titan-advance-trails/01.%20Nikagetsu-go%20no%20Kimi%20e.mp3')),
+      AudioSource.uri(Uri.parse(
+          'https://archive.org/download/attack-on-titan-advance-trails/02.%20Guren%20no%20Yumiya.mp3')),
+      AudioSource.uri(Uri.parse(
+          'https://archive.org/download/attack-on-titan-advance-trails/03.%2014-moji%20no%20Dengon.mp3')),
+      AudioSource.uri(Uri.parse(
+          'https://archive.org/download/attack-on-titan-advance-trails/04.%20Guren%20no%20Zahyou.mp3')),
+      AudioSource.uri(Uri.parse(
+          'https://archive.org/download/attack-on-titan-advance-trails/05.%20Saigo%20no%20Senka.mp3')),
+      AudioSource.uri(Uri.parse(
+          'https://archive.org/download/attack-on-titan-advance-trails/06.%20Kami%20no%20Miwaza.mp3')),
+      AudioSource.uri(Uri.parse(
+          'https://archive.org/download/attack-on-titan-advance-trails/07.%20Jiyuu%20no%20Tsubasa.mp3')),
+      AudioSource.uri(Uri.parse(
+          'https://archive.org/download/attack-on-titan-advance-trails/08.%20Souyoku%20no%20Hikari.mp3')),
+      AudioSource.uri(Uri.parse(
+          'https://archive.org/download/attack-on-titan-advance-trails/09.%20Jiyuu%20no%20Daishou.mp3')),
+      AudioSource.uri(Uri.parse(
+          'https://archive.org/download/attack-on-titan-advance-trails/10.%20Kanojo%20wa%20Tsumetai%20Hitsugi%20no%20Naka%20de.mp3')),
+      AudioSource.uri(Uri.parse(
+          'https://archive.org/download/attack-on-titan-advance-trails/11.%20Shinzou%20wo%20Sasageyo%21.mp3')),
+    ],
+  );
+  List<Map<String, dynamic>> songs = SongData().songs;
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
+  void _togglePlayPause(int index) async {
+    if (songs[index]['status'] == false) {
+      songs.forEach((song) {
+        song['status'] = false;
+      });
+      if (player.audioSource != playlist.children.indexed) {
+        await player.setAudioSource(playlist, initialIndex: index);
+      }
+      setState(() {
+        songs[index]['status'] = true;
+      });
+
+      player.play();
+    } else {
+      setState(() {
+        songs[index]['status'] = false;
+      });
+      player.pause();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -117,12 +113,13 @@ class _PlaylistState extends State<Playlist> {
                       mainAxisSize: MainAxisSize.max,
                       children: [
                         SizedBox(
-                            width: 100,
-                            height: 100,
-                            child: Image.network(
-                              songs[index]['image']!,
-                              fit: BoxFit.fill,
-                            )),
+                          width: 100,
+                          height: 100,
+                          child: Image.network(
+                            songs[index]['image']!,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
                         SizedBox(
                           width: 100,
                           height: 100,
@@ -136,15 +133,14 @@ class _PlaylistState extends State<Playlist> {
                             alignment: Alignment.centerRight,
                             child: IconButton(
                               onPressed: () {
-                                setState(() {
-                                  songs[index]['status'] =
-                                      !songs[index]['status'];
-                                });
-                                print("status: ${songs[index]['status']}");
+                                _togglePlayPause(index);
+                                setState(() {});
                               },
-                              icon: Icon(songs[index]['status']
-                                  ? Icons.play_arrow
-                                  : Icons.pause),
+                              icon: Icon(
+                                songs[index]['status']
+                                    ? Icons.pause
+                                    : Icons.play_arrow,
+                              ),
                             ),
                           ),
                         ),
@@ -157,186 +153,9 @@ class _PlaylistState extends State<Playlist> {
           ),
           DraggableScrollableSheetExample(
             songs: songs,
-          )
+          ),
         ],
       ),
-      // bottomSheet: DraggableScrollableSheetExample()
-
-      // SizedBox(
-      //   height: 100,
-      //   child: Padding(
-      //     padding: const EdgeInsets.all(10.0),
-      //     child: Row(
-      //       children: [
-      //         SizedBox(
-      //             width: 100,
-      //             height: 100,
-      //             child: Image.network(
-      //               songs[1]['image']!,
-      //               fit: BoxFit.fill,
-      //             )),
-      //         SizedBox(
-      //           width: 100,
-      //           height: 100,
-      //           child: ListTile(
-      //             title: Text(songs[1]['song']!),
-      //             subtitle: Text(songs[1]['artist']!),
-      //           ),
-      //         ),
-      //         Expanded(
-      //           child: Align(
-      //             alignment: Alignment.centerRight,
-      //             child: IconButton(
-      //               onPressed: () {
-      //                 setState(() {
-      //                   songs[1]['status'] = !songs[1]['status'];
-      //                 });
-      //                 print("status: ${songs[1]['status']}");
-      //               },
-      //               icon: Icon(
-      //                   songs[1]['status'] ? Icons.play_arrow : Icons.pause),
-      //             ),
-      //           ),
-      //         )
-      //       ],
-      //     ),
-      //   ),
-      // ),
     );
   }
 }
-
-// class Playlist extends StatelessWidget {
-//   const Playlist({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     List<Map<String, dynamic>> songs = [
-//       {
-//         'song': 'Song 1',
-//         'artist': 'Artist 1',
-//         'image': 'https://mpics.mgronline.com/pics/Images/566000000527601.JPEG',
-//         'status': false
-//       },
-//       {
-//         'song': 'Song 2',
-//         'artist': 'Artist 2',
-//         'image':
-//             'https://m.media-amazon.com/images/M/MV5BNDFjYTIxMjctYTQ2ZC00OGQ4LWE3OGYtNDdiMzNiNDZlMDAwXkEyXkFqcGdeQXVyNzI3NjY3NjQ@._V1_FMjpg_UX1000_.jpg',
-//         "status": false
-//       },
-//       {
-//         'song': 'Song 3',
-//         'artist': 'Artist 3',
-//         'image':
-//             'https://static1.srcdn.com/wordpress/wp-content/uploads/2022/01/attack-on-titan-warrior-characters-season-4.jpg',
-//         'status': false
-//       },
-//       // Add more songs here
-//     ];
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('My Playlist'),
-//         centerTitle: false,
-//       ),
-//       body: ListView.builder(
-//         itemCount: songs.length,
-//         itemBuilder: (context, index) {
-//           return Padding(
-//             padding: const EdgeInsets.all(10.0),
-//             child: Column(
-//               children: [
-//                 Row(
-//                   mainAxisSize: MainAxisSize.max,
-//                   children: [
-//                     SizedBox(
-//                         width: 100,
-//                         height: 100,
-//                         child: Image.network(
-//                           songs[index]['image']!,
-//                           fit: BoxFit.fill,
-//                         )),
-//                     SizedBox(
-//                       width: 100,
-//                       height: 100,
-//                       child: ListTile(
-//                         title: Text(songs[index]['song']!),
-//                         subtitle: Text(songs[index]['artist']!),
-//                       ),
-//                     ),
-//                     Expanded(
-//                       child: Align(
-//                         alignment: Alignment.centerRight,
-//                         child: IconButton(
-//                           onPressed: () {
-//                             songs[index]['status'] = !songs[index]['status'];
-//                           },
-//                           icon: Icon(songs[index]['status']
-//                               ? Icons.play_arrow
-//                               : Icons.pause),
-//                         ),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ],
-//             ),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
-// class DashBoard extends StatelessWidget {
-//   const DashBoard({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     List<ValueNotifier<bool>> isPlaylistSelectedNotifierList = List.generate(
-//       10,
-//       (index) => ValueNotifier<bool>(false),
-//     );
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Align(
-//             alignment: Alignment.topLeft, child: Text('My Playlist')),
-//       ),
-//       body: Column(
-//         children: [
-//           Expanded(
-//             child: ListView.builder(
-//               itemCount: 10,
-//               itemBuilder: (context, index) {
-//                 return ListTile(
-//                   title: Text('Song $index'),
-//                   subtitle: Text('Artist $index'),
-//                   leading: const Icon(Icons.music_note),
-//                   trailing: MouseRegion(
-//                     cursor: SystemMouseCursors.click,
-//                     child: GestureDetector(
-//                         onTap: () {
-//                           print('pressed');
-//                           isPlaylistSelectedNotifierList[index].value =
-//                               !isPlaylistSelectedNotifierList[index].value;
-//                         },
-//                         child: ValueListenableBuilder(
-//                           valueListenable:
-//                               isPlaylistSelectedNotifierList[index],
-//                           builder: (context, value, child) {
-//                             return Icon(
-//                                 isPlaylistSelectedNotifierList[index].value
-//                                     ? Icons.play_arrow
-//                                     : Icons.pause);
-//                           },
-//                         )),
-//                   ),
-//                 );
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
--- lib/widget/player.dart
+++ lib/widget/player.dart
@@ -2,20 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
 class Player extends StatefulWidget {
+  const Player({Key? key}) : super(key: key);
+
   @override
   _PlayerState createState() => _PlayerState();
 }
 
 class _PlayerState extends State<Player> {
-  AudioPlayer? _audioPlayer;
+  late AudioPlayer _audioPlayer;
 
   @override
   void initState() {
     super.initState();
     _audioPlayer = AudioPlayer();
 
-    // Set a sequence of audio sources that will be played by the audio player.
-    _audioPlayer!
+    _audioPlayer
         .setAudioSource(ConcatenatingAudioSource(children: [
       AudioSource.uri(Uri.parse(
           "https://archive.org/download/IGM-V7/IGM%20-%20Vol.%207/25%20Diablo%20-%20Tristram%20%28Blizzard%29.mp3")),
@@ -26,65 +27,151 @@ class _PlayerState extends State<Player> {
     ]))
         .catchError((error) {
       // catch load errors: 404, invalid url ...
-      print("An error occured $error");
+      print("An error occurred $error");
+      return Duration.zero;
     });
   }
 
   @override
   void dispose() {
-    _audioPlayer!.dispose();
+    _audioPlayer.dispose();
     super.dispose();
-  }
-
-  Widget _playerButton(PlayerState playerState) {
-    // 1
-    final processingState = playerState?.processingState;
-    if (processingState == ProcessingState.loading ||
-        processingState == ProcessingState.buffering) {
-      // 2
-      return Container(
-        margin: EdgeInsets.all(8.0),
-        width: 64.0,
-        height: 64.0,
-        child: CircularProgressIndicator(),
-      );
-    } else if (_audioPlayer!.playing != true) {
-      // 3
-      return IconButton(
-        icon: Icon(Icons.play_arrow),
-        iconSize: 64.0,
-        onPressed: _audioPlayer!.play,
-      );
-    } else if (processingState != ProcessingState.completed) {
-      // 4
-      return IconButton(
-        icon: Icon(Icons.pause),
-        iconSize: 64.0,
-        onPressed: _audioPlayer!.pause,
-      );
-    } else {
-      // 5
-      return IconButton(
-        icon: Icon(Icons.replay),
-        iconSize: 64.0,
-        onPressed: () => _audioPlayer!
-            .seek(Duration.zero, index: _audioPlayer!.effectiveIndices!.first),
-      );
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: StreamBuilder<PlayerState>(
-          stream: _audioPlayer!.playerStateStream,
-          builder: (context, snapshot) {
-            final playerState = snapshot.data;
-            return _playerButton(playerState!);
-          },
-        ),
+        child: PlayerButtons(audioPlayer: _audioPlayer),
       ),
     );
   }
+}
+
+class PlayerButtons extends StatelessWidget {
+  const PlayerButtons({
+    Key? key,
+    required this.audioPlayer,
+  }) : super(key: key);
+
+  final AudioPlayer audioPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        StreamBuilder<bool>(
+          stream: audioPlayer.shuffleModeEnabledStream,
+          builder: (context, snapshot) {
+            return _shuffleButton(context, snapshot.data ?? false);
+          },
+        ),
+        StreamBuilder<SequenceState?>(
+          stream: audioPlayer.sequenceStateStream,
+          builder: (_, __) {
+            return _previousButton();
+          },
+        ),
+        StreamBuilder<PlayerState>(
+          stream: audioPlayer.playerStateStream,
+          builder: (_, snapshot) {
+            final playerState = snapshot.data;
+            return _playPauseButton(playerState);
+          },
+        ),
+        StreamBuilder<SequenceState?>(
+          stream: audioPlayer.sequenceStateStream,
+          builder: (_, __) {
+            return _nextButton();
+          },
+        ),
+        // StreamBuilder<LoopMode>(
+        //   stream: audioPlayer.loopModeStream,
+        //   builder: (context, snapshot) {
+        //     return _repeatButton(context, snapshot.data ?? LoopMode.off);
+        //   },
+        // ),
+      ],
+    );
+  }
+
+  Widget _playPauseButton(PlayerState? playerState) {
+    final processingState = playerState?.processingState;
+    if (processingState == ProcessingState.loading ||
+        processingState == ProcessingState.buffering) {
+      return Container(
+        margin: const EdgeInsets.all(8.0),
+        width: 64.0,
+        height: 64.0,
+        child: const CircularProgressIndicator(),
+      );
+    } else if (audioPlayer.playing != true) {
+      return IconButton(
+        icon: const Icon(Icons.play_arrow),
+        iconSize: 64.0,
+        onPressed: audioPlayer.play,
+      );
+    } else if (processingState != ProcessingState.completed) {
+      return IconButton(
+        icon: const Icon(Icons.pause),
+        iconSize: 64.0,
+        onPressed: audioPlayer.pause,
+      );
+    } else {
+      return IconButton(
+        icon: const Icon(Icons.replay),
+        iconSize: 64.0,
+        onPressed: () => audioPlayer.seek(Duration.zero,
+            index: audioPlayer.effectiveIndices?.first),
+      );
+    }
+  }
+
+  Widget _shuffleButton(BuildContext context, bool isEnabled) {
+    return IconButton(
+      icon: isEnabled
+          ? Icon(
+              Icons.shuffle,
+            )
+          : const Icon(Icons.shuffle),
+      onPressed: () async {
+        final enable = !isEnabled;
+        if (enable) {
+          await audioPlayer.shuffle();
+        }
+        await audioPlayer.setShuffleModeEnabled(enable);
+      },
+    );
+  }
+
+  Widget _previousButton() {
+    return IconButton(
+      icon: const Icon(Icons.skip_previous),
+      onPressed: audioPlayer.hasPrevious ? audioPlayer.seekToPrevious : null,
+    );
+  }
+
+  Widget _nextButton() {
+    return IconButton(
+      icon: const Icon(Icons.skip_next),
+      onPressed: audioPlayer.hasNext ? audioPlayer.seekToNext : null,
+    );
+  }
+
+  // Widget _repeatButton(BuildContext context, LoopMode loopMode) {
+  //   final icons = [
+  //     const Icon(Icons.repeat),
+  //     Icon(Icons.repeat,),
+  //     Icon(Icons.repeat_one,),
+  //   ];
+  //   final index = cycleModes.indexOf(loopMode);
+  //   return IconButton(
+  //     icon: icons[index],
+  //     onPressed: () {
+  //       audioPlayer.setLoopMode(
+  //           cycleModes[(cycleModes.indexOf(loopMode) + 1) % cycleModes.length]);
+  //     },
+  //   );
+  // }
 }
